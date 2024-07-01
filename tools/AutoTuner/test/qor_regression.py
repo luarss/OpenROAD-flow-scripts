00c40b2348e15f8ef3a9a2ade7a1ac5f80e6b572
Jack Luar <39641663+luarss@users.noreply.github.com>
2024-07-01 16:23:58 +0000

    add scafold
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>

---
+++ tools/AutoTuner/test/qor_regression.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+# TODO: Automate building of 3 docker images, with 3 different Ray versions    
+# TODO: Use docker community python lib. 
+
+
+if __name__ == "__main__":
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.join(cur_dir, "../../../")
+
+
