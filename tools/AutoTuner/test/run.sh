fbac47546680d6d3a3d77af03a480cd41a9d21d1
Jack Luar <39641663+luarss@users.noreply.github.com>
2024-07-08 17:39:55 +0000

    3 docker composes
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>

---
+++ tools/AutoTuner/test/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#docker compose -f docker-compose-ray27.yaml build --no-cache
+#docker compose -f docker-compose-ray27.yaml up -d
+#
+#sleep 900
+#
+#docker compose -f docker-compose-ray28.yaml build --no-cache
+#docker compose -f docker-compose-ray28.yaml up -d
+#
+#sleep 900
+
+docker compose -f docker-compose-ray29.yaml build --no-cache
+docker compose -f docker-compose-ray29.yaml up -d
