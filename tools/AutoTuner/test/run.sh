7c2d401c5db2bda97b3e0988db9de048702d45d2
Jack Luar <39641663+luarss@users.noreply.github.com>
2024-07-11 16:30:23 +0000

    scale up expts
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>

--- tools/AutoTuner/test/run.sh
+++ tools/AutoTuner/test/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#docker compose -f docker-compose-ray27.yaml build --no-cache
-#docker compose -f docker-compose-ray27.yaml up -d
-#
-#sleep 900
-#
-#docker compose -f docker-compose-ray28.yaml build --no-cache
-#docker compose -f docker-compose-ray28.yaml up -d
-#
-#sleep 900
+docker compose -f docker-compose-ray27.yaml build --no-cache
+docker compose -f docker-compose-ray27.yaml up -d
+
+sleep 18000 # 5 hours
+
+docker compose -f docker-compose-ray28.yaml build --no-cache
+docker compose -f docker-compose-ray28.yaml up -d
+
+sleep 18000 # 5 hours
 
 docker compose -f docker-compose-ray29.yaml build --no-cache
 docker compose -f docker-compose-ray29.yaml up -d
