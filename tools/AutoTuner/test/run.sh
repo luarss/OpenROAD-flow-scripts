0eabeaa8b0c1c99ec62748c0f7a88f8b2c48aaf1
Jack Luar <39641663+luarss@users.noreply.github.com>
2024-07-14 07:16:14 +0000

    commit before run
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>

--- tools/AutoTuner/test/run.sh
+++ tools/AutoTuner/test/run.sh
@@ -3,12 +3,13 @@
 docker compose -f docker-compose-ray27.yaml build --no-cache
 docker compose -f docker-compose-ray27.yaml up -d
 
-sleep 18000 # 5 hours
+# sleep 18000 # 5 hours
+# docker compose -f docker-compose-ray27.yaml down
 
-docker compose -f docker-compose-ray28.yaml build --no-cache
-docker compose -f docker-compose-ray28.yaml up -d
+# docker compose -f docker-compose-ray28.yaml build --no-cache
+# docker compose -f docker-compose-ray28.yaml up -d
 
-sleep 18000 # 5 hours
+# sleep 18000 # 5 hours
 
-docker compose -f docker-compose-ray29.yaml build --no-cache
-docker compose -f docker-compose-ray29.yaml up -d
+# docker compose -f docker-compose-ray29.yaml build --no-cache
+# docker compose -f docker-compose-ray29.yaml up -d
