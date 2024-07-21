5f5843bee11214d390c3b76ab6f4d280c0ef1dde
Jack Luar <39641663+luarss@users.noreply.github.com>
2024-07-21 13:19:49 +0000

    add memory peak script, and more split up docker compose flow.
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>

--- tools/AutoTuner/src/autotuner/utils/helpers.py
+++ tools/AutoTuner/src/autotuner/utils/helpers.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import glob
+import re
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_DIR = os.path.join(CUR_DIR, '../../../../../flow/logs/asap7/gcd')
@@ -20,19 +21,37 @@ def load_dir(dir: str) -> pd.DataFrame:
     return df
 
 
+def peak_memory_in_dir(dir: str) -> float:
+    """
+    Return max memory usage (per mega trial) in GB. 
+    """
+    # regex for Peak memory: 185924KB.
+    regex = r"Peak memory: (\d+)KB."
+    max_memory = 0
+    for path in glob.glob(f"{dir}/*-or/*.log"):
+        with open(path) as f:
+            matches = re.findall(regex, f.readlines()[-1])
+            memory = int(matches[0]) if matches else 0
+        max_memory = max(max_memory, memory)
+    # Convert KB to GB
+    max_memory = max_memory/1000000
+    return max_memory
 
 if __name__ == "__main__":
     path = "test-tune-2024-07-07-13-45-50"
     path = os.path.join(LOG_DIR, path)
-    df = load_dir(path)
-    df.to_csv("ray27.csv", index=False)
+    print(f"Max Memory (GB): {peak_memory_in_dir(path)}")
+    # df = load_dir(path)
+    # df.to_csv("ray27.csv", index=False)
 
     path = "test-tune-2024-07-07-13-31-40"
     path = os.path.join(LOG_DIR, path)
-    df = load_dir(path)
-    df.to_csv("ray28.csv", index=False)
+    print(f"Max Memory (GB): {peak_memory_in_dir(path)}")
+    # df = load_dir(path)
+    # df.to_csv("ray28.csv", index=False)
 
     path = "test-tune-2024-07-07-13-57-06"
     path = os.path.join(LOG_DIR, path)
-    df = load_dir(path)
-    df.to_csv("ray29.csv", index=False)
+    print(f"Max Memory (GB): {peak_memory_in_dir(path)}")
+    # df = load_dir(path)
+    # df.to_csv("ray29.csv", index=False)
--- tools/AutoTuner/test/run.sh
+++ tools/AutoTuner/test/run.sh
@@ -14,3 +14,18 @@
 # docker compose -f docker-compose-ray29.yaml build --no-cache
 # docker compose -f docker-compose-ray29.yaml up -d
 # docker compose -f docker-compose-ray29.yaml down
+
+DESIGN_LIST=("gcd" "aes" "ibex")
+PLATFORM_LIST=("asap7" "sky130hd" "ihp-sg13g2")
+
+for DESIGN in "${DESIGN_LIST[@]}"; do
+    for PLATFORM in "${PLATFORM_LIST[@]}"; do
+        sed "s|{{DESIGN}}|${DESIGN}|g" .env.example > .env.tmp
+        sed "s|{{PLATFORM}}|${PLATFORM}|g" .env.tmp > .env
+        docker compose -f ray27-split.yaml build --no-cache
+        docker compose -f ray27-split.yaml up -d
+        # docker compose -f ray27-split.yaml down
+        exit 1
+        # sleep 5
+    done
+done
