f2763a194d6143e75c58a33e9693fe51ff182278
Jack Luar <jluar@precisioninno.com>
2024-09-21 16:13:26 +0000

    stash changes
    Signed-off-by: Jack Luar <jluar@precisioninno.com>

--- tools/AutoTuner/src/autotuner/utils/helpers.py
+++ tools/AutoTuner/src/autotuner/utils/helpers.py
@@ -5,13 +5,14 @@ import re
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 def load_dir(dir: str) -> pd.DataFrame:
     """
     Loads all `progress.csv` in a test-tune folder.
-    
+
     Args:
         dir: The path to the test-tune folder.
-    
+
     Returns:
         A DataFrame containing all the progress data.
     """
@@ -22,7 +23,7 @@ def load_dir(dir: str) -> pd.DataFrame:
 
 def peak_memory_in_dir(dir: str) -> float:
     """
-    Return max memory usage (per mega trial) in GB. 
+    Return max memory usage (per mega trial) in GB.
     """
     # regex for Peak memory: 185924KB.
     regex = r"Peak memory: (\d+)KB."
@@ -33,8 +34,9 @@ def peak_memory_in_dir(dir: str) -> float:
             memory = int(matches[0]) if matches else 0
         max_memory = max(max_memory, memory)
     # Convert KB to GB
-    max_memory = max_memory/1000000
+    max_memory = max_memory / 1000000
     return max_memory
+
 
 def runtime_in_dir(dir: str) -> float:
     """
@@ -45,31 +47,38 @@ def runtime_in_dir(dir: str) -> float:
     peak_runtime = df["time_total_s"].max()
     return total_runtime, peak_runtime
 
+
 if __name__ == "__main__":
-    # PROTIP: Do `rg -l <HOSTNAME>` in `./flow/logs` to find out the folder names of each trial 
+    # PROTIP: Do `rg -l <HOSTNAME>` in `./flow/logs` to find out the folder names of each trial
     # e.g. `rg -l 7dc26b0337d4`
 
-    design = "ibex"
-    platform = "ihp-sg13g2"
-    LOG_DIR = os.path.join(CUR_DIR, f'../../../../../flow/logs/{platform}/{design}/')
-    prefix = ["ray27", "ray28", "ray29"]    
-    # from a dir pick the 3 latest folders
-    paths = sorted(os.listdir(LOG_DIR))[-3:]
-    for i, path in enumerate(paths):
-        path = os.path.join(LOG_DIR, path)
-        print("----------")
-        filename = f"{prefix[i]}-{design}-{platform}.csv"
-        print("Filename:", filename)
-        print("Folder:", path)
-        print("Number of trials:", len(os.listdir(path))/2)
-        if len(os.listdir(path)) < 200:
-            print(f"Should have ~100 trials, only have {len(os.listdir(path))/2}")
-            continue
-        # Print 3 decimal places
-        print(f"Max Memory (GB): {peak_memory_in_dir(path):.3f}")
-        total_runtime, peak_runtime = runtime_in_dir(path)
-        print(f"Total runtime: {total_runtime:.3f} s, Peak runtime: {peak_runtime:.3f} s")
+    for design in ["gcd", "ibex", "aes"]:
+        for platform in ["asap7", "sky130hd", "ihp-sg13g2"]:
+            LOG_DIR = os.path.join(
+                CUR_DIR, f"../../../../../flow/logs/{platform}/{design}/"
+            )
+            prefix = ["ray27", "ray28", "ray29"]
+            # from a dir pick the 3 latest folders
+            paths = sorted(os.listdir(LOG_DIR))[-3:]
+            for i, path in enumerate(paths):
+                path = os.path.join(LOG_DIR, path)
+                print("----------")
+                filename = f"{prefix[i]}-{design}-{platform}.csv"
+                print("Filename:", filename)
+                print("Folder:", path)
+                print("Number of trials:", len(os.listdir(path)) / 2)
+                if len(os.listdir(path)) < 200:
+                    print(
+                        f"Should have ~100 trials, only have {len(os.listdir(path))/2}"
+                    )
+                    continue
+                # Print 3 decimal places
+                print(f"Max Memory (GB): {peak_memory_in_dir(path):.3f}")
+                total_runtime, peak_runtime = runtime_in_dir(path)
+                print(
+                    f"Total runtime: {total_runtime:.3f} s, Peak runtime: {peak_runtime:.3f} s"
+                )
 
-        # Export csv
-        df = load_dir(path)
-        df.to_csv(filename, index=False)
+                # Export csv
+                df = load_dir(path)
+                df.to_csv(filename, index=False)
