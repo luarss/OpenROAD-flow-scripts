25b9be9f4e1829703f62ea4a1ff5f54896faf5d9
arthurjolo <arthurjl@precisioninno.com>
2026-03-05 19:19:39 +0000

    flow: change sky130hd/ibex CORE_UTILIZATION from 45 to 50
    Signed-off-by: arthurjolo <arthurjl@precisioninno.com>

--- flow/designs/sky130hd/ibex/config.mk
+++ flow/designs/sky130hd/ibex/config.mk
@@ -15,7 +15,7 @@ export SDC_FILE      = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.
 # Adders degrade ibex setup repair
 export ADDER_MAP_FILE :=
 
-export CORE_UTILIZATION = 45
+export CORE_UTILIZATION = 50
 export PLACE_DENSITY_LB_ADDON = 0.25
 export TNS_END_PERCENT = 100
 
